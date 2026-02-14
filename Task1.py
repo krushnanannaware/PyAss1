2c26e9ff3d2e439f5aab47f6abddf0e50aeb9d97
Krushna <krushna7805@gmail.com>
2026-02-15 00:25:08 +0530

    Task 1

---
+++ Task1.py
@@ -0,0 +1,14 @@
+First=float(input("Enter the First Number: "))
+
+Second=float(input("Enter the Second Number: "))
+
+Addition=First+Second
+Subtraction=First-Second
+Mutliplication=First*Second
+Division=First/Second
+
+print("Addition : ", Addition)
+print("Subtraction : " ,Subtraction)
+print("Mutliplication : ", Mutliplication)
+print("Division : ", Division)
+
